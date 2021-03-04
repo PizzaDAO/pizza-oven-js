e66268e50e9ff29c27f10324fbb15c791d6e7dbb
Daniel van de Ghinste <danielvdg@protonmail.com>
2021-03-04 19:45:50 +0100

    added pylint, requirements.txt and main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import csv
+import json
+
+def main():
+  with open('Rare Pizzas Artists_Toppings_Team - ingredients-db.csv', mode='r') as ingredients:
+    pass
+
+if __name__ == "__main__":
+  main()
