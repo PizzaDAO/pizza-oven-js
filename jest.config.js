15f5bce25d0d484e5294961f94f5fad53acdb6a6
CryptoSpaces <cryptospaces1@gmail.com>
2021-03-06 18:10:27 +0100

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/**/*.d.ts"],
+  coveragePathIgnorePatterns: ["node_modules"],
+  coverageReporters: ["lcov", "html"],
+  preset: "ts-jest",
+  testEnvironment: "node"
+};
